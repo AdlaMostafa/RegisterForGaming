a6f2f12ba1e66b1e160283abea9e93ac7731f034
Adla Mostafa <adlamostafa422004@gmail.com>
2023-08-06 19:55:14 +0300

    Adding code for UserGuard

---
+++ src/components/Guards/UserGuard/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Navigate, Outlet} from "react-router-dom";
+import { PATHS } from "../../../router/paths";
+import { useAuthContext } from "../../../contexts/AuthContext";
+import { ROLES } from "../../../constants";
+
+const UserGuard = () =>{
+    const {role} = useAuthContext();
+    if (role === ROLES.USER) return <Outlet/>;//Outlet هو عبارة عن placeholder  بييجي مكانه متغير
+    return <Navigate to={PATHS.HOME} replace={true}/>
+};
+export default UserGuard;
