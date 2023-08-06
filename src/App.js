fb31fa6cf7ec139e2f28fdfaa92354ef8cda8cf6
Adla Mostafa <adlamostafa422004@gmail.com>
2023-08-06 20:25:35 +0300

    Adding Code for App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import SignUp from '../src/Pages/SignupPage';
+import Login from '../src/Pages/LoginPage'
+import GamePage from './Pages/GamePage';
+import { ThemeProvider } from './contexts/ThemeContext';
 
 function App() {
   return (
     <div className="App">
-      
+      <ThemeProvider><GamePage/></ThemeProvider>
+       {/* <div className={`light ${theme === dark ? "dark":'light'} `}>
+        text
+       </div> */}
+      {/* <SignUp/> */}
+       {/* <Login/> */}
     </div>
   );
 }
