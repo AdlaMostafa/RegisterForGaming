1c005170c3f04a662f6b9aa0686caa596a6cca8c
Adla Mostafa <adlamostafa422004@gmail.com>
2023-08-06 20:19:09 +0300

    Adding Code  for HomePage

---
+++ src/Pages/HomePage/index.jsx
@@ -0,0 +1,17 @@
+// HomePage.js
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      {/* Sidebar */}
+      <div>
+        <button>Logout</button>
+        <button>Toggle Theme</button>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
