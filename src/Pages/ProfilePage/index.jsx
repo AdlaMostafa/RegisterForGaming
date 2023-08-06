9f416df125af7b9ebf2fb45086ffce4123834668
Adla Mostafa <adlamostafa422004@gmail.com>
2023-08-06 20:21:04 +0300

    Adding Code  for ProfilePage

---
+++ src/Pages/ProfilePage/index.jsx
@@ -0,0 +1,15 @@
+// ProfilePage.js
+import React from "react";
+
+const ProfilePage = () => {
+  return (
+    <div>
+      <h1>Profile Page</h1>
+      {/* User Info */}
+      {/* Header */}
+      {/* Sidebar */}
+    </div>
+  );
+};
+
+export default ProfilePage;
