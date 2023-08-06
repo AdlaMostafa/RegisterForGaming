05e9f35df46fd2d239e49591eafedc32ae1deae0
Adla Mostafa <adlamostafa422004@gmail.com>
2023-08-06 20:24:05 +0300

    Adding Code Router => path.jsx

---
+++ src/router/path.js
@@ -0,0 +1,13 @@
+export const PATHS = {
+  HOME: "/", // Path for the home page
+  PROFILE: "/profile", // Path for the profile page
+  LOGIN: "/auth/login",
+  SIGNUP: "/auth/signup",
+  ADMIN: {
+    ROOT: "/admin",
+    USERS: "/admin/users",
+  },
+  ERRORS: {
+    NOT_FOUND: "/404",
+  },
+};
