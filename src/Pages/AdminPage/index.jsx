5aeff9592bd8f618b9945d585c8f87735525cea8
Adla Mostafa <adlamostafa422004@gmail.com>
2023-08-06 20:16:47 +0300

    Adding Code for AdminPage

---
+++ src/Pages/AdminPage/index.jsx
@@ -0,0 +1,13 @@
+// AdminPage.js
+import React from "react";
+
+const AdminPage = () => {
+  return (
+    <div>
+      <h1>Admin Page</h1>
+      {/* List of Users */}
+    </div>
+  );
+};
+
+export default AdminPage;
