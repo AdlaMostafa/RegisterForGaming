1bcd16cf23acdcdd153a6e1927cb325127272572
Adla Mostafa <adlamostafa422004@gmail.com>
2023-08-16 23:21:53 +0300

    Updating App.js by put a Suspense about the Router

--- src/App.js
+++ src/App.js
@@ -1,30 +1,26 @@
 import './App.css';
-import { Route,Routes } from 'react-router-dom';
-import SignUp from '../src/Pages/SignupPage';
-import Login from '../src/Pages/LoginPage'
-import GamePage from './Pages/GamePage';
+// import { Route,Routes } from 'react-router-dom';
+// import SignUp from '../src/Pages/SignupPage';
+// import Login from '../src/Pages/LoginPage'
+// import GamePage from './Pages/GamePage';
 import { ThemeProvider } from './contexts/ThemeContext';
-import ProfilePage from '../src/Pages/ProfilePage'
-import UserPage from '../src/Pages/UserPage'
+// import ProfilePage from '../src/Pages/ProfilePage'
+// import UserPage from '../src/Pages/UserPage'
 // import { Router } from 'react-router-dom';
 import Router from "./router";
 import AuthProvider from './contexts/AuthContext';
-import AdminPage from './Pages/AdminPage';
+import { Suspense } from 'react';
+import Login from './Pages/LoginPage';
+// import AdminPage from './Pages/AdminPage';
 function App() {
   return (
     <div className="App">
       <AuthProvider>
       <ThemeProvider>
-        {/* <GamePage/> */}
-      {/* <ProfilePage/>    */}
-       {/* <div className={`light ${theme === dark ? "dark":'light'} `}>
-        text
-       </div> */}
-       {/* <UserPage/> */}
-       {/* <AdminPage/> */}
-      {/* <SignUp/> */}
-       {/* <Login/> */}
+       <Suspense fallback={<h1>Loading....</h1>} >
        <Router/>
+       {/* <Login/> */}
+       </Suspense>
        </ThemeProvider>
        </AuthProvider>
     </div>
@@ -32,3 +28,12 @@ function App() {
 }
 
 export default App;
+
+
+
+{/* <GamePage/> */}
+      {/* <ProfilePage/>    */}
+       {/* <UserPage/> */}
+       {/* <AdminPage/> */}
+      {/* <SignUp/> */}
+       {/* <Login/> */}
