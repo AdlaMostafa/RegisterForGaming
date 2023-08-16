b0960fcae45d3b14c3f2b40fd8cdfc7faf1c5c7e
Adla Mostafa <adlamostafa422004@gmail.com>
2023-08-16 23:22:47 +0300

    Updating next.js by adding an ErrorBoundary around the APP

--- src/index.js
+++ src/index.js
@@ -5,14 +5,17 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import AuthProvider from './contexts/AuthContext';
 import { BrowserRouter } from 'react-router-dom';
+import ErrorBoundary from './components/ErrorBoundary';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <ErrorBoundary>
     <BrowserRouter>
     <AuthProvider>
       <App />
     </AuthProvider>
     </BrowserRouter>
+    </ErrorBoundary>
   </React.StrictMode>
 );
 
