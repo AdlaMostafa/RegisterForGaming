6bee135d7de1b72aab924801baa46cfb3ba8885f
Adla Mostafa <adlamostafa422004@gmail.com>
2023-08-17 11:21:41 +0300

    Updatint Header by adding role to navigate to profile page

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,24 +1,35 @@
-import React from 'react'
+import React from "react";
 import Profile from "../../Images/ptofile.png";
-import { Link } from 'react-router-dom';
-import { PATHS } from '../../../src/router/path';
-import './style.css'
+import { Link } from "react-router-dom";
+import { PATHS } from "../../../src/router/path";
+import "./style.css";
 
-const Header = () => {
+export const Header = () => {
+  const role = localStorage.getItem("role");
+
+  const getProfilePath = () => {
+    if (role === "admin") {
+      return PATHS.ADMIN.PROFILE;
+    } else {
+      return PATHS.PROFILE;
+    }
+  };
+
   return (
     <div className="head">
-      <div className='contentParent'>
-        <div className='contentChild'>
-        <h3 >WelcomeBack</h3>
-          <p >{localStorage.getItem('name')}</p>
+      <div className="contentParent">
+        <div className="contentChild">
+          <h3>Welcome Back</h3>
+          <p>{localStorage.getItem("name")}</p>
         </div>
-        <div className='imagediv'>
-          <Link to={PATHS.PROFILE}
-          ><img src={Profile}  alt='profile'/></Link>
-          </div>
-          </div>
+        <div className="imagediv">
+          <Link to={getProfilePath()}>
+            <img src={Profile} alt="profile" />
+          </Link>
         </div>
-  )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
