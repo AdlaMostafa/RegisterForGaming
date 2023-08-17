019f781e536d29e3cde6773d192a0c75dbf221da
Adla Mostafa <adlamostafa422004@gmail.com>
2023-08-17 11:25:45 +0300

    Updatint Profile Page

--- src/Pages/ProfilePage/index.jsx
+++ src/Pages/ProfilePage/index.jsx
@@ -1,44 +1,42 @@
 // ProfilePage.js
-import React , {useEffect} from "react";
+import React, { useEffect } from "react";
 import { ROLES } from "../../constants";
 import { useNavigate } from "react-router-dom";
-import SidePar from '../../components/SidePar'
-import Header from '../../components/Header'
-import Line from '../../Images/leftLine.png'
-import './style.css'
+import SidePar from "../../components/SidePar";
+import Header from "../../components/Header";
+import Line from "../../Images/leftLine.png";
+import "./style.css";
 import { PATHS } from "../../router/path";
-const ProfilePage = () => {
- const navigate = useNavigate();
- useEffect(() => {
-  const role = localStorage.getItem('role');
 
-  if (!role || role === ROLES.GUEST) {
-    navigate(PATHS.LOGIN);
-  }else{
-    navigate(PATHS.PROFILE)
-  }
-});
-const handleGoHome = () => {
-  navigate('/');
-};
-const userName = localStorage.getItem('name');
-return (
+const ProfilePage = () => {
+  const navigate = useNavigate();
+  const role = localStorage.getItem("role");
+  const userName = localStorage.getItem("name");
+  const userPassword = localStorage.getItem("password");
+  useEffect(() => {
+    if (!role || role === ROLES.GUEST) {
+      navigate(PATHS.LOGIN);
+    }
+  }, [navigate, role]);
+  const handleGoHome = () => {
+    navigate("/");
+  };
+  return (
     <div>
       <div className="headprof">
-        <Header/>
-        </div>
-      <input
-          value={userName}
-          type="text"
-          placeholder="Enter your name"
-        />
+        <Header />
+      </div>
+      <input value={userName} type="text" placeholder="Enter your name" />
+      {role !== ROLES.ADMIN && (
+        <input value={userPassword} type="password" readOnly />
+      )}
       <div className="col-md-2">
-          <SidePar/>
-        </div>
-        <button onClick={handleGoHome} >
-          Back to Home
-        </button>
-      <div className="line3"><img src={Line} alt="line"/></div>      
+        <SidePar />
+      </div>
+      <button onClick={handleGoHome}>Back to Home</button>
+      <div className="line3">
+        <img src={Line} alt="line" />
+      </div>
     </div>
   );
 };
